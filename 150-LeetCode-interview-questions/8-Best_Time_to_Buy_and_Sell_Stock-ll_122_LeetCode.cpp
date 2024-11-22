f105c720c23cbbc94474fa8673ae160c4ff47dd2
anish8808 <anishkashyap11111@gmail.com>
2024-11-22 13:31:04 +0530

    150-interview-8th-Solution-Leetocode

---
+++ 150-LeetCode-interview-questions/8-Best_Time_to_Buy_and_Sell_Stock-ll_122_LeetCode.cpp
@@ -0,0 +1,32 @@
+// https: // leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/description/?envType=study-plan-v2&envId=top-interview-150
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int maxProfit(vector<int> &prices)
+{
+    int buyMoney = INT_MAX;
+    int profit = INT_MIN;
+
+    for (int i = 0; i < prices.size(); i++)
+    {
+        if (prices[i] < buyMoney)
+        {
+            buyMoney = prices[i];
+        }
+        int sellMoney = prices[i] - buyMoney;
+        profit = max(profit, sellMoney);
+    }
+
+    return profit;
+}
+
+int main()
+{
+    vector<int> nums{1, 2, 3, 4, 5, 6, 7};
+    int ans = maxProfit(nums);
+    cout << "The Maximum Profit is " << ans << endl;
+
+    return 0;
+}
