1517101fd694845b275b76cfc3b88d448fb742ce
anish8808 <anishkashyap11111@gmail.com>
2024-12-21 21:20:58 +0530

    1572-Matrix-Diagonal-Sum.cpp

---
+++ 150-LeetCode-interview-questions/1572-Matrix-Diagonal-Sum.cpp
@@ -0,0 +1,31 @@
+int diagonalSum(vector<vector<int>> &mat)
+{
+    int sizes = mat.size();
+    int sum = mat[0][0];
+    for (auto i : mat)
+    {
+        for (auto j : i)
+        {
+            if (count == sizes - 2)
+            {
+                sum = sum + j;
+                count == 0;
+            }
+            else
+            {
+                count++;
+            }
+        }
+    }
+
+    return sum;
+}
+
+int main()
+{
+    vector<int, vector<int>(3)> arr
+    {
+        {1, 2, 3}, { 3, 4, 5 }
+    }
+    return 0;
+}
