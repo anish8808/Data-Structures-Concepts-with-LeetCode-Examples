818fd505988e539e1fb90879b6424ea80ef848f4
anish8808 <anishkashyap11111@gmail.com>
2025-01-19 23:38:45 +0530

    Added recursion concept

--- printArray.cpp
+++ printArray.cpp
@@ -11,6 +11,16 @@ void solve(int arr[], int index, int size)
 
     solve(arr, index + 1, size);
 }
+
+void solve2(int arr[], int index, int size)
+{
+    if (index == size)
+        return;
+
+    cout << arr[index];
+
+    solve(arr, index + 1, size);
+}
 int main()
 {
     int arr[6] = {1, 2, 3, 4, 5, 6};
