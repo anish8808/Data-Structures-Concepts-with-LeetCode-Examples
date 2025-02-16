bcf5bbdc22ff320b16c69b0d1f43f59d987176cf
anish8808 <anishkashyap11111@gmail.com>
2025-02-16 22:25:48 +0530

    Added heap question practice

--- 18.1-Heap Data Structure/14-1878. Get Biggest Three Rhombus Sums in a Grid.cpp
+++ 18.1-Heap Data Structure/14-1878. Get Biggest Three Rhombus Sums in a Grid.cpp
@@ -5,49 +5,47 @@ using namespace std;
 
 vector<int> getBiggestThree(vector<vector<int>> &grid)
 {
-    unordered_set<int> set;
+    priority_queue<int> pq;
     int m = grid.size();
     int n = grid[0].size();
-
-    int i = m / 2;
-    int j = n / 2;
-    int k = min(i, min(m - 1 - i, min(j, n - 1 - j)));
-
-    int count = 0;
-    while (count <= k)
+    for (int i = 0; i < m; i++)
     {
-        for (int i = 0; i < m; i++)
-            for (int j = 0; j < n; j++)
-            {
-                int top = grid[i - k][j];
-                int down = grid[i + k][j];
-                int left = grid[i][j - k];
-                int right = grid[i][j + k];
-
-                set.insert(top + down + left + right);
-            }
-
-        count++;
-    }
-
-    priority_queue<int, vector<int>, greater<int>> pq;
-    for (auto i : set)
-    {
-        pq.push(i);
-        while (pq.size() > 3)
+        for (int j = 0; j < n; j++)
         {
-            pq.top();
+            int sum = grid[i][j];
+            pq.push(sum);
+
+            int l = j - 1;
+            int r = j + 1;
+            int u = i - 1;
+            int d = i + 1;
+            while (l >= 0 && r < n && u >= 0 && d < m)
+            {
+                sum = grid[i][l] + grid[i][r] + grid[u][j] + grid[d][j];
+                int col1 = j - 1;
+                int col2 = j + 1;
+                for (int row1 = u + 1, row2 = d - 1; row1 < i && row2 > i; row1++, row2--)
+                {
+                    sum += grid[row1][col1] + grid[row1][col2] + grid[row2][col1] + grid[row2][col2];
+                    col1--;
+                    col2++;
+                }
+                pq.push(sum);
+
+                l--;
+                r++;
+                u--;
+                d++;
+            }
         }
     }
-
     vector<int> ans;
-    while (!pq.empty())
+    while (ans.size() != 3 && !pq.empty())
     {
-        int ele = pq.top();
-        ans.push_back(ele);
+        if (ans.size() == 0 || ans.back() != pq.top())
+            ans.push_back(pq.top());
         pq.pop();
     }
-
     return ans;
 }
 
