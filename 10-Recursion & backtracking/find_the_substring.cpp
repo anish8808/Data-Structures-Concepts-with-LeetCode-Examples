8d1796f57cee2338fdc70ac6deecefe65f96ace3
anish8808 <anishkashyap11111@gmail.com>
2025-02-18 09:11:15 +0530

    Added dp question

--- 10-Recursion & backtracking/find_the_substring.cpp
+++ 10-Recursion & backtracking/find_the_substring.cpp
@@ -2,27 +2,27 @@
 
 using namespace std;
 
-void Print_all_substring(string str , string output , int i)
+void Print_all_substring(string str, string output, int i)
 {
-	if(i>=str.length())
+	if (i >= str.length())
 	{
-		cout<<output<<endl;
+		cout << output << endl;
 		return;
 	}
-	
-	//execlude the current char 
-	Print_all_substring(str , output , i+1);
-	
-	//include the current char 
+
+	// execlude the current char
+	Print_all_substring(str, output, i + 1);
+
+	// include the current char
 	output.push_back(str[i]);
-	Print_all_substring(str , output , i+1);
+	Print_all_substring(str, output, i + 1);
 }
 
 int main()
 {
-	string str = "xy";
+	string str = "1234";
 	string output = " ";
-	int i=0;
-	Print_all_substring(str , output , i);
+	int i = 0;
+	Print_all_substring(str, output, i);
 	return 0;
 }
