f95d6b898cb5d5d61068d73bdd44585de22f7e64
anish8808 <anishkashyap11111@gmail.com>
2025-04-18 23:23:12 +0530

    cycle in a graph

--- 21-Graph/5-cycle-in-graph-undirected-using-BFS.cpp
+++ 21-Graph/5-cycle-in-graph-undirected-using-BFS.cpp
@@ -1,103 +1,59 @@
+// there is  two alogirits to detect cycle in the graph
+/*
+1 - undirected graph --2 algo
+    1- DFS
+    2- BFS
+2 - directed graph --2 algo
+    1- DFS
+    2- BFS
+*/
+
 #include <bits/stdc++.h>
 
 using namespace std;
 
 class Graph
 {
-
 public:
-    unordered_map<int, list<int>> addList;
+    unordered_map<int, list<int>> adjList;
 
-    void addEdge(int u, int v, int direction)
+    void addEdge(int u, int v, bool check)
     {
-        if (direction == 1)
+        if (check == true) // directed so in check ==1
         {
-            addList[u].push_back(v);
+            adjList[u].push_back(v);
         }
         else
         {
-            addList[u].push_back(v);
-            addList[v].push_back(u);
+            // if graph is undirected then it will travel both the side either u to v or v to u
+            adjList[u].push_back(v);
+            adjList[v].push_back(u);
         }
     }
+
     void printGraph()
     {
-        for (auto i : addList)
+        for (auto it : adjList)
         {
-            cout << i.first << " -{ ";
-            for (auto j : i.second)
+            // printing the graph
+            cout << it.first << "-->";
+            for (auto i : it.second)
             {
-                cout << j << " ";
+                cout << i << " ";
             }
-            cout << "}" << endl;
+
+            cout << endl;
         }
-    }
-
-    bool cycleDetectUndirectedBFS(int src, unordered_map<int, bool> &visited)
-    {
-        queue<int> que;
-        unordered_map<int, int> paraent;
-
-        que.push(src);
-        visited[src] = true;
-        paraent[src] = -1;
-
-        while (!que.empty())
-        {
-            int frontNode = que.front();
-            que.pop();
-            for (auto nbr : addList[frontNode])
-            {
-                if (!visited[nbr])
-                {
-                    que.push(nbr);
-                    visited[nbr] = true;
-                    paraent[nbr] = frontNode;
-                }
-                // agar koi node visited true hain aur wo node jis node k adjlist me h uska parent nahi hain
-                else if (visited[nbr] == true && nbr != paraent[frontNode])
-                {
-                    // cycle detected
-                    return true;
-                }
-            }
-        }
-        // no cycle is detected
-        return false;
     }
 };
+
 int main()
 {
-
     Graph g;
-    g.addEdge(0, 1, 0);
-    g.addEdge(0, 2, 0);
-    g.addEdge(1, 4, 0);
-
-    g.addEdge(2, 3, 0);
+    g.addEdge(0, 1, 1);
+    g.addEdge(0, 2, 1);
+    g.addEdge(1, 2, 1);
+    g.addEdge(2, 3, 1);
     g.printGraph();
-    int n = 5;
-    // this is we have to take care for disconnected graph
-    // handeling disconnected componenet is very very important
-    bool ans = false;
-    unordered_map<int, bool> visited;
-    for (int i = 0; i < n; i++)
-    {
-        if (!visited[i])
-        {
-            ans = g.cycleDetectUndirectedBFS(i, visited);
-            if (ans == true)
-                break;
-        }
-    }
-    if (ans)
-    {
-        cout << "cycle Detected " << endl;
-    }
-    else
-    {
-        cout << "cycle not detected " << endl;
-    }
-
     return 0;
 }
